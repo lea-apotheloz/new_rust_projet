7d822ca203b5d7274af1190466ea858dd3000e7e
lea-apotheloz <Lea.Apotheloz@jobtrek.ch>
2024-10-04 09:29:53 +0200

    feat: user writes a task list and writes to it in a file 4.2

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::{self,Write};
+use std::fs::OpenOptions;
+fn main() -> io::Result<()> {
+    println!("write a to-do");
+    let mut todo= String::new();
+    io::stdin().read_line(&mut todo)?;
+
+    //open the new file in append mode
+    let mut file = OpenOptions::new()
+        .create(true)
+        .append(true)
+        .open("todo.txt")?;
+    
+    // Remove white spaces at the beginning and end
+    let todo = todo.trim();
+
+    // write to text file
+    writeln!(file, "{}", todo)?;
+    Ok(())
+
 }
