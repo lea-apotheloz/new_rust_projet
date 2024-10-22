4d352c3de62ab13fc240487d274c32521733c493
lea-apotheloz <Lea.Apotheloz@jobtrek.ch>
2024-10-22 10:36:55 +0200

    feat!: change text format to json 4.5

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,42 @@
-use std::fs;
+use serde::{Deserialize, Serialize};
+use serde_json;
 use std::fs::read_to_string;
+use std::fs::{self};
 use std::io;
-fn main() -> std::io::Result<()>{
+
+#[derive(Serialize, Deserialize)]
+struct Todo {
+    message: String,
+}
+
+fn main() -> std::io::Result<()> {
     let mut todo = String::new();
     println!("write a to-do");
-    io::stdin()
-        .read_line(&mut todo)
-        .expect("Read line failed.");
-
+    io::stdin().read_line(&mut todo).expect("Read line failed.");
 
     let todo = todo.trim();
 
+    //Open file.
+    let mut todos: Vec<String> = match read_to_string("todo_list.json") {
+        Err(_) => Vec::new(),
+        Ok(todo_list) => todo_list.lines().map(String::from).collect(),
+    };
 
-   //Open file.
-   let mut todos: Vec<String> = match read_to_string("todo_list.txt") {
-       Err(_) => Vec::new(),
-       Ok(todo_list) => todo_list.lines().map(String::from).collect(),
-   };
-    
     if todo.contains("--delete") {
         let test = todo.split(" ").last();
         let nomber_line: usize = test.expect("Error").parse().unwrap();
 
         todos.remove(nomber_line - 1);
-    
-    }else {
-       // write to Vec
+    } else {
+        // write to Vec
         todos.push(todo.to_string());
-
     }
-    fs::write("todo_list.txt", todos.join("n")).expect("err");
+
+    fs::write(
+        "todo_list.json",
+        serde_json::to_string(&todos).expect("Cannot serialize"),
+    )
+    .expect("Cannot write files");
+
     Ok(())
 }
